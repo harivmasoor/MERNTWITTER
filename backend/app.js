dad2ee1d75a1079a1cc90fea6a42e3ed8cd28c62
harivmasoor <harimasoor@gmail.com>
2023-08-27 13:55:31 -0700

    phase 1 and 2

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+// backend/app.js
+
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
+
+const app = express();
+
+app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(express.json()); // parse JSON request body
+app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
+app.use(cookieParser()); // parse cookies as an object on req.cookies
+
+// Attach Express routers
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
+
+module.exports = app;
+
+
